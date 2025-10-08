b98fb2af14cc8ebe2d137cb949b8c4680aa9ebbe
nahcub <kbugang0321@gmail.com>
2025-10-09 02:01:02 +0900

    refine CompileSdk version in build.gradle.kts and agp  version in libs.versions.toml

--- Application/AIDocent/app/build.gradle.kts
+++ Application/AIDocent/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.airis"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.airis"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
